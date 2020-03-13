4440b08a54ffe417d776bc91d77ecc4b5a342fdb
ManojkumarKB <manojbhaskaran1998@gmail.com>
2020-03-13 15:02:06 +0530

    UptoLogin

--- src/main/java/com/webapp/studmark/repository/UserRepository.java
+++ src/main/java/com/webapp/studmark/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.webapp.studmark.repository;
 
 //import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.webapp.studmark.model.User;
 public interface UserRepository extends JpaRepository<User,Integer> {
 	
+	@Query(value="SELECT *FROM user WHERE username=?1 and password=?2", nativeQuery = true)
 	public User findByUsernameAndPassword(String username,String password);
 }
